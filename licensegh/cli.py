88535d9aa3cc7dfc7efb8d933b3a1334a531f7fc
Saúl Piña <sauljabin@gmail.com>
2021-09-17 14:29:42 -0500

    adding version aption

--- licensegh/cli.py
+++ licensegh/cli.py
@@ -1,5 +1,7 @@
 import click
 
+from licensegh import __version__
+
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
@@ -7,12 +9,15 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 @click.option("--print", "-p", is_flag=True, help="Print the license file")
 @click.option("--search", "-s", is_flag=True, help="Search license, shows a list")
 @click.option("--list", "-l", is_flag=True, help="List all found licenses")
+@click.version_option(__version__)
 @click.argument("id", metavar="<id>", nargs=1, required=False)
 def main(id, print, search, list):
     """
-    licensegh is a command line tool that generates a license file for a project from the github open source lincese templates.
+    licensegh is a command line tool that generates a license file for a project
+    from the github open source lincese templates.
 
-    licensegh <id> generates a LICENSE file using the license id listed in the github open source license templates.
+    licensegh <id> generates a LICENSE file using the license id listed in the
+    github open source license templates.
     """
     click.echo(f"Generating license for {id}")
     click.echo(f"Printing license: {print}")
