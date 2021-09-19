f252701db61b98dec30c89f28e5b95d5c6f9293c
Saúl Piña <sauljabin@gmail.com>
2021-09-19 16:52:36 -0500

    ignoring some files at test coverage

--- scripts/tests-coverage.py
+++ scripts/tests-coverage.py
@@ -10,8 +10,6 @@ def main():
             "run",
             "coverage",
             "run",
-            "--source",
-            "licensegh",
             "-m",
             "unittest",
             "-v",
